7a4bd698d5d36fe29f0dac476a43947b6d45a78d
leo rodrigues <leofrrodrigues86@gmail.com>
2024-11-22 20:06:52 -0300

    Pastas para o front e back implementadas

---
+++ back-end/UPA-digital/src/main/java/com/squad13/UPA_digital/UpaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.squad13.UPA_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpaDigitalApplication.class, args);
+	}
+
+}
---
+++ back-end/UPA-digital/src/test/java/com/squad13/UPA_digital/UpaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad13.UPA_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
