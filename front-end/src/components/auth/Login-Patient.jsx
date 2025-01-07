791fba54c19a13b5af8ee9f3814923e287f54306
leonardo <leofrrodrigues86@gmail.com>
2025-01-07 11:40:40 -0300

    Sub-rotas dentro da página patient implementadas com sucesso!

--- front-end/src/components/auth/Login-Patient.jsx
+++ front-end/src/components/auth/Login-Patient.jsx
@@ -5,10 +5,33 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-function LoginPatient(){
-    const URL_API = 'http://localhost:8080/api/v1/login/login';
+const loginSchema = z.object({
+    cartaoSus: z
+        .string()
+        .length(15, "O cartão do SUS deve conter exatamente 15 números")
+        .regex(/^\d+$/, "O cartão do SUS deve conter apenas números"),
+    password: z.string().min(6, "A senha deve conter pelo menos 6 caracteres"),
+});
+
+const URL_API = "http://localhost:8080/api/v1/login/login";
+
+function LoginPatient() {
+    const navigate = useNavigate();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            cartaoSus: "",
+            password: "",
+        },
+    });
+
     const onSubmit = async (data) => {
-        try {
+        /*  try {
             // Fazer a requisição POST para o back-end
             const response = await axios.post(`${URL_API}`, {
                 username: data.cpf,
@@ -29,40 +52,14 @@ function LoginPatient(){
             // Exibir mensagem de erro caso o login falhe
             // setErrMsg('Falha no login, verifique as credenciais');
             
-        }
-    }
-    
+        } */
+        navigate("/patient/home");
+    };
 
-    // Crie o schema de validação APÓS definir activeLogin
-    const loginSchema = z.object({
-        cartaoSus: z
-            .string()
-            .length(15, "O cartão do SUS deve conter exatamente 15 números")
-            .regex(/^\d+$/, "O cartão do SUS deve conter apenas números"),
-        password: z.string().min(6, "A senha deve conter pelo menos 6 caracteres"),
-
-    });
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            cartaoSus: "",
-            password: "",
-        }
-    });
-    const navigate = useNavigate()
-
-   
-    return(
+    return (
         <div className="flex justify-center items-center  bg-gray-100">
             <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
                 <h1 className="text-2xl font-bold text-center text-blue-700 mb-6">Login SUS</h1>
-
-               
 
                 <form
                     onSubmit={handleSubmit(onSubmit)}
@@ -78,16 +75,11 @@ function LoginPatient(){
                                 type="text"
                                 {...register("cartaoSus")}
                                 className={`w-full px-4 py-3 bg-gray-50 border-2 rounded-xl 
-                                ${errors.cartaoSus
-                                    ? "border-red-400"
-                                    : "border-gray-200"
-                                }`}
+                                ${errors.cartaoSus ? "border-red-400" : "border-gray-200"}`}
                                 placeholder="Digite seu número do cartão"
                             />
                             {errors.cartaoSus && (
-                                <div className="text-red-500 text-sm mt-2">
-                                    {errors.cartaoSus.message}
-                                </div>
+                                <div className="text-red-500 text-sm mt-2">{errors.cartaoSus.message}</div>
                             )}
                         </div>
 
@@ -100,16 +92,11 @@ function LoginPatient(){
                                 type="password"
                                 {...register("password")}
                                 className={`w-full px-4 py-3 bg-gray-50 border-2 rounded-xl 
-                                ${errors.password
-                                    ? "border-red-400"
-                                    : "border-gray-200"
-                                }`}
+                                ${errors.password ? "border-red-400" : "border-gray-200"}`}
                                 placeholder="Digite sua senha"
                             />
                             {errors.password && (
-                                <div className="text-red-500 text-sm mt-2">
-                                    {errors.password.message}
-                                </div>
+                                <div className="text-red-500 text-sm mt-2">{errors.password.message}</div>
                             )}
                         </div>
                         <button
@@ -123,7 +110,7 @@ function LoginPatient(){
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default LoginPatient
+export default LoginPatient;
--- front-end/src/pages/patient/index.jsx
+++ front-end/src/pages/patient/index.jsx
@@ -1,91 +1,91 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { cardPatient } from "@/data/cardPatient";
 import { Button } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import PaginationComponent from "@/components/pagination";
 import { useState } from "react";
 import { consultasMarcadas } from "@/data/tableConsultasMarcadas";
+import { Outlet, useLocation } from "react-router-dom";
+
+const validRoutes = [
+    "/patient/home/all-doctors",
+    "/patient/home/health-posts",
+    "/patient/home/make-appointment",
+    "/patient/home/calendar",
+    "/patient/home/perfil",
+];
 
 const HomePagePatient = () => {
-  const ITEMS_PER_PAGE = 10;
-  const [currentPage, setCurrentPage] = useState(1);
+    const ITEMS_PER_PAGE = 10;
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const visibleItems = consultasMarcadas.slice(
-    startIndex,
-    startIndex + ITEMS_PER_PAGE
-  );
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const visibleItems = consultasMarcadas.slice(startIndex, startIndex + ITEMS_PER_PAGE);
 
-  const totalPages = Math.ceil(consultasMarcadas.length / ITEMS_PER_PAGE);
-  return (
-    <div>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3 mb-6">
-        {cardPatient.map((info) => (
-          <Card key={info.id}>
-            <CardHeader>
-              <CardTitle>{info.title}</CardTitle>
-              <CardDescription>{info.especialidade}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm">{info.description}</p>
-            </CardContent>
-            <CardFooter>
-              <Button>Marcar Consulta</Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+    const totalPages = Math.ceil(consultasMarcadas.length / ITEMS_PER_PAGE);
 
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Especialidade</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Unidade</TableHead>
-              <TableHead>Data</TableHead>
-              <TableHead>Horário</TableHead>
-              <TableHead>Infos</TableHead>
-            </TableRow>
-          </TableHeader>
+    const location = useLocation();
+    const urlPath = location.pathname;
 
-          <TableBody>
-            {visibleItems.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell className="font-medium">
-                  {item.especialidade}
-                </TableCell>
-                <TableCell>{item.status}</TableCell>
-                <TableCell>{item.posto}</TableCell>
-                <TableCell>{item.data}</TableCell>
-                <TableCell>{item.horario}</TableCell>
-                <TableCell className="cursor-pointer">Saiba mais</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <PaginationComponent
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={setCurrentPage}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3 mb-6">
+                {cardPatient.map((info) => (
+                    <Card key={info.id}>
+                        <CardHeader>
+                            <CardTitle>{info.title}</CardTitle>
+                            <CardDescription>{info.especialidade}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p className="text-sm">{info.description}</p>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Marcar Consulta</Button>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
+
+            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+                {validRoutes.includes(urlPath) ? (
+                    <Outlet />
+                ) : (
+                    <>
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Especialidade</TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Unidade</TableHead>
+                                    <TableHead>Data</TableHead>
+                                    <TableHead>Horário</TableHead>
+                                    <TableHead>Infos</TableHead>
+                                </TableRow>
+                            </TableHeader>
+
+                            <TableBody>
+                                {visibleItems.map((item, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell className="font-medium">{item.especialidade}</TableCell>
+                                        <TableCell>{item.status}</TableCell>
+                                        <TableCell>{item.posto}</TableCell>
+                                        <TableCell>{item.data}</TableCell>
+                                        <TableCell>{item.horario}</TableCell>
+                                        <TableCell className="cursor-pointer">Saiba mais</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <PaginationComponent
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            onPageChange={setCurrentPage}
+                        />
+                    </>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default HomePagePatient;
