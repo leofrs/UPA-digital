a2e8f89eca2c3c3656493098a51f0c7520c54ecb
LuizEduardo <156156838+LuizEduardoSantosOliveira@users.noreply.github.com>
2025-03-07 20:16:18 -0300

    unificado as paginas de login

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,37 +1,140 @@
 import React, { useState } from "react";
-import { AppSidebar } from "@/components/app-sidebar";
-import LoginPatient from "@/components/auth/Login-Patient";
-import LoginDoctor from "@/components/auth/Login-Doctor";
-import LoginRecepptionist from "@/components/auth/Login-Recepptionist";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useAuth } from "../context/authContext";
+
 
 function LoginPage() {
-    const infos = [{ id: 1, nome: "patient" }, { id: 2, nome: "doctor" }, { id: 3, nome: "recepptionist" }];
-    const [open, setOpen] = useState("patient");
+    const URL_API = 'http://localhost:8080/api/v1/login/login';
+    const navigate = useNavigate()
+    const loginSchema = z.object({
+        email: z
+            .string()
+            .regex(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "O email precisa ser válido"),
+        password: z.string().min(6, "A senha deve conter pelo menos 6 caracteres"),
 
-    return (
-        <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-            {/* Renderiza os botões de login para os tipos de usuário */}
-            <div className="flex justify-center mb-2">
-                {infos.map((type) => (
+    });
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            
+        }
+    });
+    const onSubmit = async (data) => {
+        /*try {
+            // Fazer a requisição POST para o back-end
+            const response = await axios.post(`${URL_API}/login`, {
+                username: data.cpf,
+                password: data.password,
+                crm: data.crm
+            });
+
+            // Armazenar o token no localStorage
+            localStorage.setItem('token', response.data.token);
+            
+            // Definir o cabeçalho Authorization para as próximas requisições
+            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+            
+            // Redirecionar para a página do doutor
+            navigate("/doctor/home");
+
+        } catch (err) {
+            // Exibir mensagem de erro caso o login falhe
+            setErrMsg('Falha no login, verifique as credenciais');
+            console.error(err);
+        }*/
+
+        navigate("")
+    }
+
+    
+
+
+    
+    return(
+        <div className="flex justify-center items-center  bg-gray-100 pt-20">
+        <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+            <h1 className="text-2xl font-bold text-center text-blue-700 mb-6">Login SUS</h1>
+
+          
+
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="w-full max-w-md p-8 bg-white rounded-2xl shadow-2xl border border-gray-100"
+            >
+                <div className="space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-600 mb-2">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="text"
+                            {...register("email")}
+                            className={`w-full px-4 py-3 bg-gray-50 border-2 rounded-xl 
+                            ${errors.email
+                                ? "border-red-400"
+                                : "border-gray-200"
+                            }`}
+                            placeholder="Digite seu número do cartão"
+                        />
+                        {errors.cpf && (
+                            <div className="text-red-500 text-sm mt-2">
+                                {errors.email.message}
+                            </div>
+                        )}
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-600 mb-2">
+                            Senha
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            {...register("password")}
+                            className={`w-full px-4 py-3 bg-gray-50 border-2 rounded-xl 
+                            ${errors.password
+                                ? "border-red-400"
+                                : "border-gray-200"
+                            }`}
+                            placeholder="Digite sua senha"
+                        />
+                        {errors.password && (
+                            <div className="text-red-500 text-sm mt-2">
+                                {errors.password.message}
+                            </div>
+                        )}
+                    </div>
+
+                     
+                    
+
+                    
+                        
+                    
+
                     <button
-                        onClick={() => setOpen(type.nome)} // Altera o tipo de usuário selecionado
-                        key={type.id}
-                        className={`${
-                            open === type.nome ? 'bg-blue-500 text-white' : 'bg-blue-300 text-white'
-                        } px-4 py-2 rounded-md mr-2 mb-2 transition-colors hover:opacity-90`}
+                        type="submit"
+                        className="w-full py-3 px-4 bg-blue-600 text-white font-bold rounded-xl 
+                        hover:bg-green-700 transition-all duration-300"
                     >
-                        {type.nome}
+                        Entrar
                     </button>
-                ))}
-            </div>
-
-            {/* Exibe o formulário de login correspondente ao tipo de usuário selecionado */}
-            {open === "patient" ? <LoginPatient /> : open === "doctor" ? <LoginDoctor /> : <LoginRecepptionist />}
-            
-            
-           
+                </div>
+            </form>
         </div>
-    );
+    </div>
+    )
 }
 
-export default LoginPage;
+export default LoginPage
